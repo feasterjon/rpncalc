648608bd9e11e6f23d7df89802f5ac09f4ac7ae4
Jon Feaster <feaster@gmail.com>
2024-01-31 16:11:39 -0600

    Scrap index.json

--- src/components/rpncalc/keyboard.js
+++ src/components/rpncalc/keyboard.js
@@ -1,12 +1,9 @@
 import './index.css';
-import configMain from './index.json';
 import { useEffect, useState } from 'react';
 
 function Keyboard({ config = {} }) {
 
-  const configKeyboard = configMain.data.attributes.keyboard;
-
-  const cols = config.cols || configKeyboard.defaults.cols,
+  const cols = config.cols,
     currentInput = config.currentInput,
     [dynamicStyles, setDynamicStyles] = useState(''),
     idKeyboard = config.id || 'main',
@@ -14,10 +11,165 @@ function Keyboard({ config = {} }) {
     setCurrentInput = config.setCurrentInput,
     vibrateEnabled = (typeof window.navigator.vibrate === 'function') ? true : false;
 
-  const buttons = configKeyboard.data[idKeyboard].map((button) => ({
+  const configButtons = {
+    data: {
+      fn: [
+        {
+          id: 1,
+          label: '\u221a',
+          type: 'fn',
+          value: 's'
+        },
+        {
+          id: 2,
+          type: 'fn',
+          value: '^'
+        },
+        {
+          id: 3,
+          label: '\u03C0',
+          type: 'fn',
+          value: 'p'
+        },
+        {
+          id: 4,
+          type: 'fn',
+          value: 'e'
+        },
+        {
+          id: 5,
+          label: '\u03C6',
+          type: 'fn',
+          value: 'f'
+        }
+      ],
+      main: [
+        {
+          id: 1,
+          label: 'AC',
+          type: 'del',
+          value: 'Delete'
+        },
+        {
+          id: 2,
+          label: '\u232B',
+          type: 'operator',
+          value: 'Backspace'
+        },
+        {
+          id: 3,
+          label: 'ANS',
+          type: 'operator',
+          value: 'a'
+        },
+        {
+          id: 4,
+          label: '\u00F7',
+          type: 'operator',
+          value: '/'
+        },
+        {
+          id: 5,
+          value: '7'
+        },
+        {
+          id: 6,
+          value: '8'
+        },
+        {
+          id: 7,
+          value: '9'
+        },
+        {
+          id: 8,
+          label: '\u00D7',
+          type: 'operator',
+          value: '*'
+        },
+        {
+          id: 9,
+          value: '4'
+        },
+        {
+          id: 10,
+          value: '5'
+        },
+        {
+          id: 11,
+          value: '6'
+        },
+        {
+          id: 12,
+          type: 'operator',
+          value: '-'
+        },
+        {
+          id: 13,
+          value: '1'
+        },
+        {
+          id: 14,
+          value: '2'
+        },
+        {
+          id: 15,
+          value: '3'
+        },
+        {
+          id: 16,
+          type: 'operator',
+          value: '+'
+        },
+        {
+          id: 17,
+          value: '0'
+        },
+        {
+          id: 18,
+          label: '\u00B7',
+          value: '.'
+        },
+        {
+          id: 19,
+          label: 'SPC',
+          value: ' '
+        },
+        {
+          id: 20,
+          label: '\u23ce',
+          type: 'enter',
+          value: 'Enter'
+        }
+      ]
+    },
+    styles: { // active is repeated in main to ensure active styles are applied onClick
+      del: {
+        active: 'bg-secondary',
+        main: 'active:bg-secondary bg-secondary-light border-2 border-secondary-light'
+      },
+      enter: {
+        active: 'bg-primary',
+        main: 'active:bg-primary bg-primary-light border-2 border-primary-light'
+      },
+      fn: {
+        active: 'bg-gray-400',
+        main: 'active:bg-gray-400 border-2 border-gray-300'
+      },
+      main: {
+        active: 'bg-gray-400',
+        main: 'active:bg-gray-400 bg-gray-300 border-2 border-gray-300'
+      },
+      operator: {
+        active: 'bg-primary',
+        main: 'active:bg-primary border-2 border-primary-light'
+      }
+    }
+  };
+
+  const buttons = configButtons.data[idKeyboard].map((button) => ({
     id: button.id,
     label: button.label || button.value,
-    styles: configKeyboard.styles[button.type] || configKeyboard.styles.main,
+    styles: configButtons.styles[button.type] || configButtons.styles.main,
     type: button.type,
     value: button.value
   }));
@@ -52,7 +204,9 @@ function Keyboard({ config = {} }) {
   };
 
   return (
-    <div className={`grid grid-cols-${cols}`}>
+    <div className={`grid
+      ${cols ? `grid-cols-${cols}` : `grid-cols-4`}
+    `}>
       {buttons.map((button) =>
         <div
           className={`cursor-default font-bold m-2 p-2 rounded-full select-none text-center
