c9714c69b7b9b5fddc0b01cff69dcf0c2cddf61a
Jon Feaster <feaster@gmail.com>
2024-03-13 10:29:25 -0500

    Adjust horizontal layouts for Tablets

--- src/components/Elements/Keyboard/Keyboard.jsx
+++ src/components/Elements/Keyboard/Keyboard.jsx
@@ -46,47 +46,61 @@ export function Keyboard(props) {
   }, [buttons, setCurrentInput]);
 
   return (
-    <div className="flex flex-wrap items-center justify-center lg:aspect-square lg:w-auto sm:w-11/12">
-      {buttons.map((button) =>
-        <div
-          className={`
-            ${button.type === 'fn' ? 'basis-1/5' : 'basis-1/4'}
-            ${button.order}
-            sm:basis-1/5
-          `}
-          key={`button-${button.id}`}
-        >
-          <button
+    <div className="
+      flex
+      lg:aspect-square
+      lg:w-auto
+      sm:w-full
+      w-auto
+    ">
+      <div className="
+        flex
+        flex-wrap
+        items-center
+        justify-center
+        w-full
+      ">
+        {buttons.map((button) =>
+          <div
             className={`
-              aspect-square
-              cursor-pointer
-              dark:text-neutral-100
-              flex
-              font-bold
-              items-center
-              justify-center
-              lg:aspect-square
-              mx-auto
-              my-[0.125rem]
-              rounded-full
-              select-none
-              size-3/4
-              sm:aspect-auto
-              sm:size-11/12
-              text-center
-              text-neutral-900
-              text-2xl
-              ${button.styles.etc}
-              ${`button-${button.id}` === `button-${pressedKey}` ? dynamicStyles : button.styles.main ? button.styles.main : ''}
+              ${button.type === 'fn' ? 'basis-1/5' : 'basis-1/4'}
+              ${button.order}
+              sm:basis-1/5
             `}
-            aria-label={button.label}
-            onClick={() => handleInput(button)}
-            tabIndex={-1}
+            key={`button-${button.id}`}
           >
-            {button.icon ? <Icon id={button.icon} /> : button.label}
-          </button>
-        </div>
-      )}
+            <button
+              className={`
+                aspect-square
+                cursor-pointer
+                dark:text-neutral-100
+                flex
+                font-bold
+                items-center
+                justify-center
+                lg:aspect-square
+                mx-auto
+                my-[0.125rem]
+                rounded-full
+                select-none
+                size-3/4
+                sm:aspect-auto
+                sm:size-11/12
+                text-center
+                text-neutral-900
+                text-2xl
+                ${button.styles.etc}
+                ${`button-${button.id}` === `button-${pressedKey}` ? dynamicStyles : button.styles.main ? button.styles.main : ''}
+              `}
+              aria-label={button.label}
+              onClick={() => handleInput(button)}
+              tabIndex={-1}
+            >
+              {button.icon ? <Icon id={button.icon} /> : button.label}
+            </button>
+          </div>
+        )}
+      </div>
     </div>
   );
 }
