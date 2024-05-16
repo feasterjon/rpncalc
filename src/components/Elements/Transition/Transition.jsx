922bb6a7b1284d3c7231a09e53bf5472ce9a2205
Jon Feaster <feaster@gmail.com>
2024-05-16 16:53:48 -0500

    TypeScript Migration: Transition

--- src/components/Elements/Transition/Transition.jsx
+++
@@ -1,9 +0,0 @@
-import styles from './Transition.module.css';
-
-export function Transition(props) {
-  return (
-    <div className={`${props.show ? styles.fadeVisible : styles.fadeHidden}`}>
-      {props.children}
-    </div>
-  );
-}
