36a4415842420cf7c6e3e6443bf7393ca10df1bc
Jon Feaster <feaster@gmail.com>
2024-06-03 17:01:56 -0500

    Added Help test

--- src/types/help.ts
+++ src/types/help.ts
@@ -1,11 +1,11 @@
 import type { Table } from './table';
 
 export type Help = {
-  footer: string;
+  footer?: string;
   sections: {
     data: { data: string | Table }[];
     heading?: string;
     id: number;
   }[];
-  title: string;
+  title?: string;
 };
